37ca21af6000344f02cc1fff7fcb817ec459334d
Davina Pinto <davinapinto@gmail.com>
2026-03-31 12:30:53 +1100

    remove key

--- config/firebase.js
+++ config/firebase.js
@@ -9,7 +9,14 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 
 // Initialize Firebase
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyAoUirYqK0d9_E92MO0uqzdNisLGbE1X8Q",
+  authDomain: "trackerapp-b5664.firebaseapp.com",
+  databaseURL: "https://trackerapp-b5664-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "trackerapp-b5664",
+  storageBucket: "trackerapp-b5664.firebasestorage.app",
+  messagingSenderId: "601546372984",
+  appId: "1:601546372984:web:b47dabf5c3648bb14f7a8d",
+  measurementId: "G-FEGTLXNQJX"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
